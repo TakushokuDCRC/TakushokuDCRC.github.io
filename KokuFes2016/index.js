0e28150cce9d3c6bfe9a1db87ca2f71b63804aed
MICKeyzwo <singing.programmer@gmail.com>
2016-06-05 21:59:38 +0900

    tenaoshi
    iro iro tenaoshi

--- KokuFes2016/index.js
+++ KokuFes2016/index.js
@@ -1,7 +1,12 @@
 ﻿var vis;
 window.onload = function(){
 	vis = false;
-}
+	if(window.innerWidth <= 480){
+		youtubeReady("movie_smart");
+		deleteElement("movie_switch");
+		deleteElement("movie_frame");
+	}
+};
 window.onscroll = function(){
 	if(vis){
 		window.scrollTo(sx, sy);
@@ -10,13 +15,24 @@ window.onscroll = function(){
 		sy = window.scrollY;
 	}
 }
-function youtubeReady(){
-	if(typeof(YT) !=  "undefined"){
-		var wi = window.innerWidth * 0.6;
-		var he = 9 / 16 * wi;
-		document.getElementById("batsu").style.top = (he * -1) + "px";
+function youtubeReady(div_id){
+	if(typeof(YT) != "undefined"){
+		var wi;
+		var he;
+		if(window.innerWidth > window.innerHeight){
+			if(window.innerHeight < window.innerWidth * 9 / 16){
+				he = window.innerHeight * 0.9;
+				wi = he * 16 / 9;
+			}else{
+				wi = window.innerWidth * 0.8;
+				he = wi * 9 / 16;
+			}
+		}else{
+			wi = window.innerWidth * 0.9;
+			he = wi * 9 / 16;
+		}
 		ytPlayer = new YT.Player(
-			"movie_div",
+			div_id,
 			{
 				width: wi,
 				height: he,
@@ -34,14 +50,13 @@ function setVisible(){
 	player.style.top = window.scrollY + "px";
 	player.style.left = window.scrollX + "px";
 	player.style.display = "block";
-	youtubeReady();
+	youtubeReady("movie_div");
 }
 function setHidden(){
 	vis = false;
 	document.body.style.overflow = "visible";
 	deleteElement("movie_div");
-	deleteElement("batsu");
-	document.getElementById("movie_td").innerHTML += "<div id='movie_div'></div><a href='javascript:void(0)' id='batsu'><span>×</span></a>";
+	document.getElementById("movie_td").innerHTML += "<div id='movie_div'></div>";
 	document.getElementById("movie_frame").style.display = "none";
 }
 function deleteElement(id){
