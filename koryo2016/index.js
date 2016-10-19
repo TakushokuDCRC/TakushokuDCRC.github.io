a91cff016a0d84458165d00853f0f7a765381762
MICKeyzwo <singing.programmer@gmail.com>
2016-10-20 01:28:54 +0900

    youtube player define
    I defined youtube player using youtube iframe api.

--- koryo2016/index.js
+++ koryo2016/index.js
@@ -3,16 +3,38 @@ function changeBgColor( id, color ){
 }
 
 function ChangeTab(tabname) {
-   
+
    document.getElementById('tab1').style.display = 'none';
    document.getElementById('tab2').style.display = 'none';
    document.getElementById('tab3').style.display = 'none';
    document.getElementById('tab4').style.display = 'none';
    document.getElementById('tab5').style.display = 'none';
-   
+
    document.getElementById(tabname).style.display = 'block';
 }
 
 function link() {
 	document.getElementById(hoge);
 }
+
+window.addEventListener("load", function(){
+  //youtube player define
+  var ytPlayer;
+  function youtubeReady(){
+    if(YT){
+      width = window.innerWidth * 0.8;
+      height = width * 9 / 16;
+      ytPlayer = new YT.Player(
+        "youtube",
+        {
+          width: width,
+          height: height,
+          videoId: "QSxvDkKYv_I"
+        }
+      )
+    }else{
+      document.getElementById("youtube").innerHTML = "Youtube iframe API が見つかりませんでした";
+    }
+  }
+  youtubeReady();
+});
