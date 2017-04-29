b3128c45f82b2440cd3c8aa5597e65192f74f98a
poikelp <takukelp@gmail.com>
2017-04-30 00:09:17 +0900

    Poi enter a war

---
+++ members/Yussiy/fristScript.js
@@ -0,0 +1,30 @@
+﻿function ChangeTab(tabname) {
+
+	   // タブメニュー実装
+	   
+	   document.getElementById('tab1').style.display = 'none';
+
+	   document.getElementById('tab2').style.display = 'none';
+	   document.getElementById('tab3').style.display = 'none';
+
+	   document.getElementById('tab4').style.display = 'none';
+
+	   // タブメニュー実装
+	   
+	   document.getElementById(tabname).style.display = 'block';
+}
+
+myWeek=new Array("Sun","Mon","Tue","Wed","Thu","Sun","Sat");
+function myTime(){
+     myDate=new Date();
+     myMsg = myDate.getFullYear() + "/";
+     myMsg += ( myDate.getMonth() + 1 ) + "/";
+     myMsg += myDate.getDate() + "/";
+     myMsg += myWeek[myDate.getDay()] + " ";
+     myMsg += myDate.getHours() + ":";
+     myMsg += myDate.getMinutes() + ":";
+     myMsg += myDate.getSeconds();
+     document.getElementById("myIDdate").innerHTML = myMsg;
+}
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
---
+++ plugin/Packery/script.js
@@ -0,0 +1,27 @@
+﻿var pckry = new Packery( '.grid', {
+  // options
+  itemSelector: '.grid-item',
+  gutter: 10
+});
+
+function changeMember(className){
+  var elements = document.getElementsByClassName("member");
+  for(var i = 0 ; i < elements.length; i++){
+    elements[i].style.display = "none";
+  }
+  if(className == "check-all"){
+    for(var i = 0 ; i < elements.length; i++){
+      elements[i].style.display = "block";
+    }
+  }else{
+    elements = document.getElementsByClassName(className.replace("check-",""));
+    for(var i = 0 ; i < elements.length; i++){
+      elements[i].style.display = "block";
+    }
+  }
+	pckry.layout();
+}
+
+window.onload = function(){
+  changeMember("check-all");
+}
